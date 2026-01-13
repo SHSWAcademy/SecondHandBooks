6da94fce1e137fc54b637e2514c8113fe1d09af7
nippyclouding <wonderwall09in@naver.com>
2026-01-13 17:23:24 +0900

    작업7
    # Conflicts:
    #	src/main/java/project/trade/TradeVO.java

--- src/main/java/project/member/MemberController.java
+++ src/main/java/project/member/MemberController.java
@@ -3,6 +3,10 @@ package project.member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import project.trade.TradeVO;
 
 @Controller
 @Slf4j
@@ -10,5 +14,6 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
 
     private final MemberService memberService;
+
 
 }
--- src/main/java/project/trade/TradeService.java
+++ src/main/java/project/trade/TradeService.java
@@ -1,4 +1,22 @@
 package project.trade;
 
-public interface TradeService {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TradeService {
+    private final TradeMapper tradeMapper;
+
+    public TradeVO findBySeq(long trade_seq) {
+        TradeVO findTrade = tradeMapper.findBySeq(trade_seq);
+        findTrade.setImgUrls(tradeMapper.findImgUrl(trade_seq));
+        return findTrade;
+    }
 }
