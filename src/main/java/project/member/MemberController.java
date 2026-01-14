8a7505485baa2b32e3b724b9cbb8dd466d2393fb
rbxo3213 <rbxo3213@naver.com>
2026-01-14 12:42:00 +0900

    memberController update

--- src/main/java/project/member/MemberController.java
+++ src/main/java/project/member/MemberController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Slf4j
@@ -22,8 +24,32 @@ public class MemberController {
     @PostMapping("/auth/signup")
     public String signUp(MemberVO vo, Model model) {
         boolean result = memberService.signUp(vo);
+        if (result) {
+            model.addAttribute("msg", "회원가입 되었습니다.");
+            model.addAttribute("cmd", "move");
+            model.addAttribute("url", "login");
+        } else {
+            model.addAttribute("msg", "회원가입에 실패했습니다.");
+            model.addAttribute("cmd", "back");
+        }
+        return "common/return";
+    }
 
+    @GetMapping("/auth/ajax/idCheck")
+    @ResponseBody
+    public int idCheck(@RequestParam String login_id) {
+        return memberService.idCheck(login_id);
+    }
 
-        return null;
+    @GetMapping("/auth/ajax/emailCheck")
+    @ResponseBody
+    public int emailCheck(@RequestParam String member_email) {
+        return memberService.emailCheck(member_email);
+    }
+
+    @GetMapping("/auth/ajax/nicknmCheck")
+    @ResponseBody
+    public int nicknmCheck(@RequestParam String member_nicknm) {
+        return memberService.nickNmCheck(member_nicknm);
     }
 }
