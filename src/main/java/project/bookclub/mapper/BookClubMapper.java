5763bee9dc73bcdb94cb466bd6fb0ee3c734d897
doyeon.danny <doyeon.danny@gmail.com>
2026-01-14 12:05:24 +0900

    fix : add comments in VO

--- src/main/java/project/bookclub/mapper/BookClubMapper.java
+++ src/main/java/project/bookclub/mapper/BookClubMapper.java
@@ -1,7 +1,11 @@
 package project.bookclub.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import project.bookclub.vo.BookClubVO;
+
+import java.util.List;
 
 @Mapper
 public interface BookClubMapper {
+    List<BookClubVO> selectAll();
 }
--- src/main/java/project/bookclub/service/BookClubService.java
+++ src/main/java/project/bookclub/service/BookClubService.java
@@ -1,5 +1,15 @@
 package project.bookclub.service;
 
-public interface BookClubService {
+import project.bookclub.vo.BookClubVO;
 
+import java.util.List;
+
+public interface BookClubService {
+    // #1. 독서 모임 메인 페이지
+    // #1-1. 독서모임 만들기
+    List<BookClubVO> searchBookClubs(BookClubSearchCondition condition);
+
+    // #1-2. 독서모임 검색
+
+    // #1-3. 전체 모임 리스트 출력
 }
