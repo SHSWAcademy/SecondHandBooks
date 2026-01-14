6c957d769218f9beb4a01da78c8f41852d28f77e
doyeon.danny <doyeon.danny@gmail.com>
2026-01-14 15:15:34 +0900

    feat : add bookclub main page's mapper, service

--- src/main/java/project/bookclub/service/BookClubService.java
+++ src/main/java/project/bookclub/service/BookClubService.java
@@ -1,5 +1,42 @@
 package project.bookclub.service;
 
-public interface BookClubService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.bookclub.mapper.BookClubMapper;
+import project.bookclub.vo.BookClubVO;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookClubService {
+
+    private final BookClubMapper bookClubMapper;
+    /*
+    * #1. 독서모임 메인 페이지
+    */
+    // #1-1. 전체 독서모임 리스트 조회
+    List<BookClubVO> getBookClubList() {
+        return bookClubMapper.searchAll();
+    }
+
+    // #1-2. 독서모임 검색
+    public List<BookClubVO> searchBookClubs(String keyword) {
+        // 키워드 없으면 전체 검색
+        if (keyword == null || keyword.isBlank()) {
+            return bookClubMapper.searchAll();
+        }
+        List<String> tokens = Arrays.stream(keyword.split("\\s+"))
+                                    .filter(StringUtils::hasNext)
+                                    .toList();
+
+        // 키워드 검색 SQL
+        return bookClubMapper.searchByKeyword();
+    }
+
+    // #1-3. 독서모임 생성 가능 여부 : 로그인 여부, (추후) 생성 개수 제한, 권한
+    public boolean canCreateBookClub(Long memberId) {
+        // 비로그인 시 생성 불가
+        return memberId != null;
+    }
 }
