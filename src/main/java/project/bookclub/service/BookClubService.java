d4450778720dd98d4f98eb339b7797737e5be1e7
doyeon.danny <doyeon.danny@gmail.com>
2026-01-14 16:45:54 +0900

    fix: remove invalid bookclubMapper.xml files

--- src/main/java/project/bookclub/service/BookClubService.java
+++ src/main/java/project/bookclub/service/BookClubService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import project.bookclub.mapper.BookClubMapper;
 import project.bookclub.vo.BookClubVO;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 @Service
 @RequiredArgsConstructor
@@ -16,22 +18,29 @@ public class BookClubService {
     * #1. 독서모임 메인 페이지
     */
     // #1-1. 전체 독서모임 리스트 조회
-    List<BookClubVO> getBookClubList() {
+    public List<BookClubVO> getBookClubList() {
         return bookClubMapper.searchAll();
     }
 
     // #1-2. 독서모임 검색
     public List<BookClubVO> searchBookClubs(String keyword) {
         // 키워드 없으면 전체 검색
-        if (keyword == null || keyword.isBlank()) {
+//        if (keyword == null || keyword.isBlank()) {
+//            return bookClubMapper.searchAll();
+//        }
+
+        List<String> tokens = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(keyword);
+        while(st.hasMoreTokens()) {
+            tokens.add(st.nextToken());
+        }
+
+        if (tokens.isEmpty()) {
             return bookClubMapper.searchAll();
         }
-        List<String> tokens = Arrays.stream(keyword.split("\\s+"))
-                                    .filter(StringUtils::hasNext)
-                                    .toList();
 
         // 키워드 검색 SQL
-        return bookClubMapper.searchByKeyword();
+        return bookClubMapper.searchByKeyword(tokens);
     }
 
     // #1-3. 독서모임 생성 가능 여부 : 로그인 여부, (추후) 생성 개수 제한, 권한
