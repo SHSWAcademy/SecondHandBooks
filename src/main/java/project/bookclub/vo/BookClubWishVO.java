88e633d6b33b4c88e04b84289c1ae1cefc38c85d
hot-gamja <coke0701@gmail.com>
2026-01-14 15:16:56 +0900

    fix: /vo

--- src/main/java/project/bookclub/vo/BookClubWishVO.java
+++ src/main/java/project/bookclub/vo/BookClubWishVO.java
@@ -6,9 +6,9 @@ import java.time.LocalDateTime;
 
 @Data
 public class BookClubWishVO {
-    private Long book_club_seq; // pk, fk
-    private Long book_club_wish_seq; // pk
-    private Long member_seq; // fk
-    private LocalDateTime crt_dtm;
+    private Long book_club_seq; // 독서모임 ID, PK, FK
+    private Long book_club_wish_seq; // 독서모임 찜 ID, PK
+    private Long member_seq; // 멤버 ID, FK
+    private LocalDateTime crt_dtm; // 찜 생성 일시
 
 }
