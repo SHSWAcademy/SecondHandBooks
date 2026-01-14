2017f3850d37da95780f616fcc4120a91ef0de4f
hot-gamja <coke0701@gmail.com>
2026-01-14 15:16:56 +0900

    fix: /vo

--- src/main/java/project/bookclub/vo/BookClubRequestVO.java
+++ src/main/java/project/bookclub/vo/BookClubRequestVO.java
@@ -7,12 +7,12 @@ import java.time.LocalDateTime;
 
 @Data
 public class BookClubRequestVO {
-    private Long book_club_seq;
-    private Long book_club_request_seq;
-    private Long request_member_seq;
+    private Long book_club_seq; // 독서모임 ID, FK
+    private Long book_club_request_seq; // 독서모임 가입 요청 ID
+    private Long request_member_seq; // 가입을 요청한 멤버 ID
 
-    private String request_cont;
-    private String request_st;
-    private LocalDateTime request_dtm;
-    private LocalDate request_processed_dt;
+    private String request_cont; // 가입 요청 시 작성한 내용
+    private String request_st; // 가입 요청의 상태, ENUM(WAIT / APPROVED / REJECTED)
+    private LocalDateTime request_dtm; // 가입 요청 시간
+    private LocalDate request_processed_dt; // 가입 요청이 처리된 시간
 }
