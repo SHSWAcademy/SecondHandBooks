0c2b9d4f498130dbe52736c595047a767b6981a8
le-30 <lleesh36@gmail.com>
2026-01-14 11:11:46 +0900

    jsp 수정

--- src/main/java/project/member/MemberMapper.java
+++ src/main/java/project/member/MemberMapper.java
@@ -4,5 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-
+    int idCheck(String login_id);
+    int nickNmCheck(String member_nicknm);
+    int emailCheck(String member_email);
 }
--- src/main/java/project/member/MemberService.java
+++ src/main/java/project/member/MemberService.java
@@ -2,6 +2,7 @@ package project.member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService{
+
+    @Autowired
     private final MemberMapper memberMapper;
+
+    public int idCheck(String login_id) {
+        return memberMapper.idCheck(login_id);
+    }
+    public int nickNmCheck(String member_nicknm) {
+        return memberMapper.nickNmCheck(member_nicknm);
+    }
+    public int emailCheck(String member_email) {
+        return memberMapper.emailCheck(member_email);
+    }
 }
