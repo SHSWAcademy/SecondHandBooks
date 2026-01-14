1f3557d928669f3c439052fbf563b5bb27ff7328
hot-gamja <coke0701@gmail.com>
2026-01-14 15:33:11 +0900

    #10 first commit

--- src/main/java/project/bookclub/controller/BookClubController.java
+++ src/main/java/project/bookclub/controller/BookClubController.java
@@ -1,8 +1,9 @@
-package project.bookclub;
+package project.bookclub.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import project.bookclub.service.BookClubService;
 
 @Controller
 @Slf4j
