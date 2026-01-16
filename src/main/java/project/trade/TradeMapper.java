2a35e73412bfd6507cb7f6bc0abccad99725ff1b
nippyclouding <wonderwall09in@naver.com>
2026-01-16 15:58:32 +0900

    페이지네이션

--- src/main/java/project/trade/TradeMapper.java
+++ src/main/java/project/trade/TradeMapper.java
@@ -7,11 +7,16 @@ import java.util.List;
 
 @Mapper
 public interface TradeMapper {
+
     List<TradeVO> findAll(); // 전체조회
+    List<TradeVO> findAllWithPaging(@Param("limit") int limit, @Param("offset") int offset);
+    int countAll();
+
     TradeVO findBySeq(long trade_seq);  // 상세조회
     List<TradeImageVO> findImgUrl(long trade_seq);    // 이미지 url 조회
     int save(TradeVO tradeVO);  // 판매글 등록
     int update(TradeVO tradeVO); // 판매글 수정
-
     int delete(@Param("trade_seq") Long tradeSeq);
+
+
 }
