653228fa75b21c9fc01c73ebc50cd7c4b48392f8
BGCHOI <chlqjarms1@gmail.com>
2026-01-19 12:18:19 +0900

    ìµ병합

--- src/main/java/project/trade/TradeVO.java
+++ src/main/java/project/trade/TradeVO.java
@@ -62,13 +62,10 @@ public class TradeVO {
                 book_img != null && !book_img.equals("") &&
                 book_title != null && !book_title.equals("") &&
                 book_author != null && !book_author.equals("") &&
-                book_publisher
-
-
-                        != null && !book_publisher.equals("") &&
-                isbn != null && !isbn.equals("") &&
+                book_publisher != null && !book_publisher.equals("") &&
                 category_nm != null && !category_nm.equals("") &&
-                sale_cont != null && !sale_cont.equals("");
+                sale_cont != null && !sale_cont.equals("") &&
+                isbn != null && !isbn.equals("");
     }
 
     public BookVO generateBook () {
