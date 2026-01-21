23432205ccb9dfb2c2d981cb19e5a1151ac0c8d3
rbxo3213 <rbxo3213@naver.com>
2026-01-21 16:11:09 +0900

    admin페이지 초기

--- src/main/java/project/admin/AdminService.java
+++ src/main/java/project/admin/AdminService.java
@@ -2,6 +2,11 @@ package project.admin;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.bookclub.BookClubVO;
+import project.member.MemberVO;
+import project.trade.TradeVO;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +17,14 @@ public class AdminService {
     public AdminVO login(String id, String pwd) {
         return adminMapper.loginAdmin(id, pwd);
     }
+
+    // 통계
+    public int countAllMembers() { return adminMapper.countAllMembers(); }
+    public int countAllTrades() { return adminMapper.countAllTrades(); }
+    public int countAllBookClubs() { return adminMapper.countAllBookClubs(); }
+
+    // 목록
+    public List<MemberVO> getRecentMembers() { return adminMapper.selectRecentMembers(); }
+    public List<TradeVO> getRecentTrades() { return adminMapper.selectRecentTrades(); }
+    public List<BookClubVO> getRecentBookClubs() { return adminMapper.selectRecentBookClubs(); }
 }
