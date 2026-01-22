315912215980eddc4f6e133bfa66a804f900a7d6
rbxo3213 <rbxo3213@gmail.com>
2026-01-22 15:14:08 +0900

    admin page data load

--- src/main/java/project/admin/AdminService.java
+++ src/main/java/project/admin/AdminService.java
@@ -71,4 +71,36 @@ public class AdminService {
         data.put("categories", adminMapper.selectCategoryStats());
         return data;
     }
+
+    // 목록
+    public List<MemberVO> getRecentMembers() { return adminMapper.selectRecentMembers(); }
+    public List<TradeVO> getRecentTrades() { return adminMapper.selectRecentTrades(); }
+    public List<BookClubVO> getRecentBookClubs() { return adminMapper.selectRecentBookClubs(); }
+
+    // 조회
+    public List<LoginInfoVO> getMemberLoginLogs() {
+        return adminMapper.selectMemberLoginLogs();
+    }
+    public List<LoginInfoVO> getAdminLoginLogs() {
+        return adminMapper.selectAdminLoginLogs();
+    }
+
+    // 관리자 로그인 기록
+    public void recordAdminLogin(Long admin_seq, String login_ip) {
+        adminMapper.insertAdminLogin(admin_seq, login_ip);
+    }
+    // 관리자 로그아웃 기록
+    public void recordAdminLogout(Long admin_seq, String logout_ip) {
+        adminMapper.updateAdminLogout(admin_seq, logout_ip);
+    }
+
+    // 회원 로그인 기록
+    public void recordMemberLogin(Long member_seq, String login_ip) {
+        adminMapper.insertMemberLogin(member_seq, login_ip);
+    }
+    //회원 로그아웃 기록
+    public void recordMemberLogout(Long member_seq, String logout_ip) {
+        adminMapper.updateMemberLogout(member_seq, logout_ip);
+    }
+
 }
