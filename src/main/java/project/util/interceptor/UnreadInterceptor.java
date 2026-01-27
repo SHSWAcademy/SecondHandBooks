938dd6b93b237f158359bcc33372f3b99611c295
BGCHOI <chlqjarms1@gmail.com>
2026-01-27 11:22:44 +0900

    modifiy main / chat

--- src/main/java/project/util/interceptor/UnreadInterceptor.java
+++ src/main/java/project/util/interceptor/UnreadInterceptor.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import project.chat.message.MessageService;
+import project.member.MemberVO;
+import project.util.Const;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,9 +19,11 @@ public class UnreadInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        if (modelAndView != null && messageService.isUnreadMessage()) {
-            modelAndView.addObject("messageSign", true);
+        if (modelAndView != null) {
+            MemberVO member = (MemberVO) request.getSession().getAttribute(Const.SESSION);
+            if (member != null && messageService.isUnreadMessage(member.getMember_seq())) {
+                modelAndView.addObject("messageSign", true);
+            }
         }
     }
-
 }
