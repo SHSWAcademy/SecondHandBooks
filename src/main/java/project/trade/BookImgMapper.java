2a212bbf4623ab7e1d4bb86e6fc5f8806c16b5d0
nippyclouding <wonderwall09in@naver.com>
2026-01-27 15:17:01 +0900

    merge

--- src/main/java/project/trade/BookImgMapper.java
+++ src/main/java/project/trade/BookImgMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Mapper
 public interface BookImgMapper {
     int save(@Param("imgUrl") String imgUrl, @Param("trade_seq") long trade_seq);
-    List<String> findImgUrl(long tradeSeq);
+    List<String> findImgUrl(@Param("trade_seq") long trade_seq);
 
     int deleteBySeq(@Param("trade_seq") Long tradeSeq);
 }
