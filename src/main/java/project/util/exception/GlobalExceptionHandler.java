1e4a26348d235495d98f7cff1178dda1a5796aa4
nippyclouding <wonderwall09in@naver.com>
2026-01-28 16:11:48 +0900

    merge
    # Conflicts:
    #	src/main/webapp/WEB-INF/views/trade/tradeform.jsp

--- src/main/java/project/util/exception/GlobalExceptionHandler.java
+++ src/main/java/project/util/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,5 +25,12 @@ public class GlobalExceptionHandler {
     public String handleGenericException(Exception e, Model model) {
         model.addAttribute("errorMessage", "서버 오류가 발생했습니다.");
         return "error/500";
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxSizeException(MaxUploadSizeExceededException ex, Model model) {
+        // FlashAttributes 대신 Model에 바로 넣기
+        model.addAttribute("errorMessage", "업로드 가능한 파일 크기를 초과했습니다. (최대 5MB)");
+        return "error/fileSizeError"; // 리다이렉트 하지 않고 바로 JSP 렌더
     }
 }
