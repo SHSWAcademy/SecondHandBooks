eac9576a2e18634046aa89ba9bae532a37f4a9b4
le-30 <lleesh36@gmail.com>
2026-01-29 09:11:20 +0900

    로그 수정중

---
+++ src/main/java/project/common/UserType.java
@@ -0,0 +1,16 @@
+package project.common;
+
+public enum UserType {
+    ADMIN(10), // 10초 후 로그아웃
+    MEMBER(300); // 5분(300초) 후 로그아웃
+
+    private final int timeoutSeconds;
+
+    UserType(int timeoutSeconds) {
+        this.timeoutSeconds = timeoutSeconds;
+    }
+
+    public int getTimeoutSeconds() {
+        return timeoutSeconds;
+    }
+}
---
+++ src/main/java/project/config/interceptor/MemberActivityInterceptor.java
@@ -0,0 +1,61 @@
+package project.config.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import project.common.LogoutPendingManager;
+import project.common.UserType;
+import project.member.MemberVO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MemberActivityInterceptor implements HandlerInterceptor {
+
+    private final LogoutPendingManager logoutPendingManager;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        String uri = request.getRequestURI();
+
+        //logout-pending API는 제외
+        if (uri.contains("logout-pending")) {
+            return true;
+        }
+
+        HttpSession sess = request.getSession(false);
+
+        if (sess != null) {
+            MemberVO memberVO = (MemberVO) sess.getAttribute("loginSess");
+
+            if (memberVO != null) {
+                // ★ 강제 로그아웃 체크 ★
+                if (logoutPendingManager.isForceLogout(UserType.MEMBER, memberVO.getMember_seq())) {
+                    // 강제 로그아웃 대상에서 제거
+                    logoutPendingManager.removeForceLogout(UserType.MEMBER, memberVO.getMember_seq());
+
+                    // 세션 무효화
+                    sess.invalidate();
+
+                    log.info("Member 강제 로그아웃 실행: memberSeq={}", memberVO.getMember_seq());
+
+                    // 홈으로 리다이렉트 (또는 로그인 페이지)
+                    response.sendRedirect("/");
+                    return false;
+                }
+                // pending 상태 제거 (활동 감지)
+                logoutPendingManager.removePending(UserType.MEMBER, memberVO.getMember_seq());
+
+            }
+        }
+        return true;
+    }
+}
+
