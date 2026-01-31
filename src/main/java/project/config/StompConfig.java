6765cd6abfb106918c1d8ae6468a137b6edd00c2
nippyclouding <wonderwall09in@naver.com>
2026-02-01 03:02:14 +0900

    fix: WebSocket CORS 허용 설정 추가

--- src/main/java/project/config/StompConfig.java
+++ src/main/java/project/config/StompConfig.java
@@ -19,6 +19,8 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chatEndPoint")
-                .addInterceptors(new HttpSessionHandshakeInterceptor()).withSockJS();
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .withSockJS();
     }
 }
