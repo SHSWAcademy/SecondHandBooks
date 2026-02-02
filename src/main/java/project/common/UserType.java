5a00765d81ebe47d31ab771e9f61b43fb5cfdb23
le-30 <lleesh36@gmail.com>
2026-02-02 12:29:53 +0900

    회원로그 수정 완료

--- src/main/java/project/common/UserType.java
+++ src/main/java/project/common/UserType.java
@@ -1,8 +1,8 @@
 package project.common;
 
 public enum UserType {
-    ADMIN(1),   // 10초 후 로그아웃
-    MEMBER(1); // 5분(300초) 후 로그아웃
+    ADMIN(1),
+    MEMBER(60);
 
     private final int timeoutSeconds;
 
